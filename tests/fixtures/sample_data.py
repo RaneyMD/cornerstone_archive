7590b9d20422ad2b6add60d8d6924732bb333cb5
RaneyMD <michael+github@raneyworld.com>
2026-02-04 11:48:14 +0200

    test: add sample data fixtures

---
+++ tests/fixtures/sample_data.py
@@ -0,0 +1,22 @@
+"""Sample data for testing."""
+
+SAMPLE_CONTAINER = {
+    'container_id': 1,
+    'issue_id': 1,
+    'ia_identifier': 'americanarchitect_1876_05',
+    'acquisition_status': 'pending'
+}
+
+SAMPLE_ISSUE = {
+    'issue_id': 1,
+    'title_id': 1,
+    'volume': 19,
+    'number': 5,
+    'year_published': 1876
+}
+
+SAMPLE_PUBLICATION_FAMILY = {
+    'family_id': 1,
+    'family_name': 'American_Architect_family',
+    'description': 'American Architect & Building News'
+}
